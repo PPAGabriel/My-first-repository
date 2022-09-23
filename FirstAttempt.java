dfb55251aed91e3670ecc44c0ec321148280e784
Gabriel Alejandro Perez Pereira <gperezpereira@danielcastelao.org>
2022-09-23 09:19:27 +0200

    Otro comentario añadido en el archivo java

--- FirstAttempt.java
+++ FirstAttempt.java
@@ -2,6 +2,10 @@
  * Updating the code just 4fun
  */
 
+/**
+ * Another piece of comments from me
+ */
+
 public class FirstAttempt {
     public static void main(String[] args) {
         System.out.println("Message in a bottle");
